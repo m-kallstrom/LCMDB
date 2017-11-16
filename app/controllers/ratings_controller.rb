9b7611fd614aa8e116d0490d5bbe79fe3dc0776e
Marla Kallstrom <brandykou@gmail.com>
2017-11-16 02:19:08 -0600

    enable changing vote but prevent ballot stuffing

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,13 +1,22 @@
 class RatingsController < ApplicationController
 
   def create
-    @rating = Rating.new(ratings_params)
-    if @rating.save
+    @rating = Rating.find_by(user_id: current_user.id, movie_id: ratings_params[:movie_id])
+    if @rating
+      if @rating.value == ratings_params[:value]
+        flash[:notice] = "You've already voted. Stop trying to stuff the ballot box."
+      else
+        @rating.update_attributes(value: ratings_params[:value])
+      end
       redirect_to "/movies/#{@rating.movie_id}"
     else
-      status 422
-      @errors = @rating.errors.full_messages
-      redirect_to new_rating_path
+      @rating = Rating.new(ratings_params)
+      if @rating.save
+        redirect_to "/movies/#{@rating.movie_id}"
+      else
+        flash[:notice] = @rating.errors.full_messages
+        redirect_back
+      end
     end
   end
 
