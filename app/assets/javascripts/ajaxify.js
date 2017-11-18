239781bd7a05860b75335aac2a597195b845e891
Marla Kallstrom <brandykou@gmail.com>
2017-11-17 23:41:13 -0600

    method that counts number of words entered and displays feedback to the user

---
+++ app/assets/javascripts/ajaxify.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+
+  //Update the word count as the user types a review
+  $('#reviewtextarea').on("keyup", function(event){
+    var $reviewbox = $(this);
+    var currentText = $reviewbox.val();
+    var wordCount = currentText.split(" ").length;
+    var counter = 5 - wordCount
+    var plurals
+
+    if (counter === 1) {
+      plurals = " word"
+    } else {
+      plurals = " words"
+    }
+
+    if (counter >= 1) {
+      $('#ratingHelpBlock').text("You have " + counter + plurals + " remaining.");
+
+    } else if (counter === 0 ) {
+      $('#ratingHelpBlock').text("That's the perfect number of words right there.");
+
+    } else {
+      $('#ratingHelpBlock').text("Now you've gone too far. " + counter);
+    }
+
+  }) //type in review area
+
+
+}) //doc ready
