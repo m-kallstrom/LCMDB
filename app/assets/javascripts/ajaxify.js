4f5ac809906aa69eaf5cffa5ad8a04e2656e78dc
Marla Kallstrom <brandykou@gmail.com>
2017-11-18 20:25:25 -0600

    illumnation was not a little too much

--- app/assets/javascripts/ajaxify.js
+++ app/assets/javascripts/ajaxify.js
@@ -5,26 +5,26 @@ $(document).ready(function(){
     var $reviewbox = $(this);
     var currentText = $reviewbox.val();
     var wordCount = currentText.split(" ").length;
-    var counter = 5 - wordCount
-    var plurals
+    var counter = 5 - wordCount;
+    var plurals;
 
     if (counter === 1) {
       plurals = " word"
     } else {
       plurals = " words"
-    }
+    };
 
     if (counter >= 1) {
-      $('#ratingHelpBlock').text("You have " + counter + plurals + " remaining.");
+      $('#ratingHelpBlock').text("You have " + counter + plurals + " remaining");
 
     } else if (counter === 0 ) {
-      $('#ratingHelpBlock').text("That's the perfect number of words right there.");
+      $('#ratingHelpBlock').text("That's the perfect number of words right there");
 
     } else {
       $('#ratingHelpBlock').text("Now you've gone too far. " + counter);
-    }
+    };
 
-  }) //type in review area
+  }); //type in review area
 
   //highlight and animate bell ratings
   new BellRating();
